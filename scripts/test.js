d35b35e1bd1c26167d472632e3f30f13a5a8f7c9
Chris Kirby <christian.h.kirby@gmail.com>
2018-12-31 22:11:04 -0900

    Updating this after so many months

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,42 @@
-$(document).ready(function() {
-    $(".dropdown").hover(function() {
-        $(".dropdown-content").fadeToggle("slow");
-    });
-    $(function() {
-        $(document.body).fadeIn(1000);
-    });
+const words = ['I am a developer.', "I am a veteran.", "I am a gamer."];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('typing').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 200);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('typing').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 200);
+	};
+	loopDeleting();
+};
+
+
+$(document).ready(function(){
+    typingEffect();
 });
